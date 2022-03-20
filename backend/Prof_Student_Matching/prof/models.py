906741a4f0840cbefc913a0a963a22c56bcfe1f4
Navneet <navneet.agarwal2058@gmail.com>
2022-03-21 00:32:53 +0530

    Minor changes

--- backend/Prof_Student_Matching/prof/models.py
+++ backend/Prof_Student_Matching/prof/models.py
@@ -1,13 +1,6 @@
 from django.db import models
 from django import forms
 
-FIELD_CHOICES =(
-    ("AI", "Artificial Intelligence"),
-    ("Sys", "Systems"),
-    ("Bio", "Bio Sciences"),
-    ("Mth", "Mathematics"),
-)    
-
 # Create your models here.
 class prof(models.Model):
     first_name = models.CharField(max_length=30)
@@ -15,17 +8,11 @@ class prof(models.Model):
     des = models.CharField(max_length=30)
     #des for designation
     lab_name = models.CharField(max_length=30)
-    # res_interest = forms.MultipleChoiceField()
+    res_interest = models.JSONField(null=True,help_text="This includes the field in which the prof is interested")
 
 class project(models.Model):
-    FIELD_CHOICES =(
-        ("AI", "Artificial Intelligence"),
-        ("Sys", "Systems"),
-        ("Bio", "Bio Sciences"),
-        ("Mth", "Mathematics"),
-    )    
     title = models.CharField(max_length=30)
     descr = models.CharField(max_length=400)
     time_req = models.IntegerField() # Number of months req for project 
-    tech_stack = models.CharField(max_length=100)
-    # field = forms.MultipleChoiceField(choice = FIELD_CHOICES)
+    tech_stack = models.JSONField(null=True,help_text="This includes the tech stack requirement for project")
+    res_interest = models.JSONField(null=True,help_text="This includes the broad research field for a project")
--- backend/Prof_Student_Matching/student/models.py
+++ backend/Prof_Student_Matching/student/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 # Create your models here.
 class student(models.Model):
-    first_name = models.CharField(max_length=30)
-    sec_name = models.CharField(max_length=30)
-    cgpa = models.FloatField(null=False)
-    roll_no = models.IntegerField(null=False)
+    first_name = models.CharField(max_length=30,help_text="First Name")
+    sec_name = models.CharField(max_length=30,help_text="Second Name")
+    cgpa = models.FloatField(null=False,help_text="Student's CGPA")
+    roll_no = models.IntegerField(null=False,help_text="Student's Roll number")
     #des for designation
-
+    res_interest = models.JSONField(null=True,help_text="This includes the field in which the student is interested")
+    
