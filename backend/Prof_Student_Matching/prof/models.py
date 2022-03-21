c307d2c54fa8d079abc3ee51ec881256d8693eff
Navneet <navneet.agarwal2058@gmail.com>
2022-03-21 18:09:38 +0530

    minor changes

--- backend/Prof_Student_Matching/prof/models.py
+++ backend/Prof_Student_Matching/prof/models.py
@@ -3,16 +3,24 @@ from django import forms
 
 # Create your models here.
 class prof(models.Model):
+    id = models.AutoField(primary_key=True)
     first_name = models.CharField(max_length=30)
     sec_name = models.CharField(max_length=30)
     des = models.CharField(max_length=30)
     #des for designation
     lab_name = models.CharField(max_length=30)
     res_interest = models.JSONField(null=True,help_text="This includes the field in which the prof is interested")
+    stud_list = models.JSONField(null= True, help_text="This includes the list of students roll no which have worked under a particular prof")
+
 
 class project(models.Model):
+    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=30)
     descr = models.CharField(max_length=400)
     time_req = models.IntegerField() # Number of months req for project 
     tech_stack = models.JSONField(null=True,help_text="This includes the tech stack requirement for project")
     res_interest = models.JSONField(null=True,help_text="This includes the broad research field for a project")
+    sel_stud = models.JSONField(null=True,help_text="Roll number of the students selected for this project")
+    alloc_stat = models.BooleanField(default=False,help_text="This helps to determine if the project has been allocated to students")
+    req_stu_no = models.IntegerField(null=False,help_text="This helps in determining the number of students required for a project")
+    advisor_id = models.JSONField(null=False,help_text="This stores the advisor id's for this project")
--- backend/Prof_Student_Matching/student/models.py
+++ backend/Prof_Student_Matching/student/models.py
@@ -5,7 +5,7 @@ class student(models.Model):
     first_name = models.CharField(max_length=30,help_text="First Name")
     sec_name = models.CharField(max_length=30,help_text="Second Name")
     cgpa = models.FloatField(null=False,help_text="Student's CGPA")
-    roll_no = models.IntegerField(null=False,help_text="Student's Roll number")
+    roll_no = models.IntegerField(null=False,help_text="Student's Roll number",primary_key=True)
     #des for designation
     res_interest = models.JSONField(null=True,help_text="This includes the field in which the student is interested")
     
