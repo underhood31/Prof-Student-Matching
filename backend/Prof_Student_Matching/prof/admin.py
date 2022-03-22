2ac33747fa3367c85f27f11a85b5ccc660abbf87
Navneet <navneet.agarwal2058@gmail.com>
2022-03-22 16:54:11 +0530

    Registered research fields

--- backend/Prof_Student_Matching/prof/admin.py
+++ backend/Prof_Student_Matching/prof/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Prof,Project
+from .models import Prof,Project,ResearchField
 # Register your models here.
 
 admin.site.register(Prof)
 admin.site.register(Project)
+admin.site.register(ResearchField)
